ba4f3332a3fc6692039a5e7c2d96d8c110ed45f8
helene-cpu <66snahel@stud.akademiet.no>
2026-01-26 12:10:07 +0000

    Laget databaseforbindelse i app.py

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
-from flask import Flask, render_template
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask import Flask, render_template, redirect import mysql.connector
+from forms import RegisterForm, LoginForm
+
 
 app= Flask(__name__)
+app.secret_key= "passord"
+
+def get_conn():
+    return mysql.connector.connect(
+        host="localhost",
+        user="booking"
+        password="Strkno321!"
+        database="booking_system"
+    )
+
 
 @app.route('/')
 def index():
